64a84d036820f6b4aba07770f36122d42e602363
Franck Leijen <143752138+Francienepien@users.noreply.github.com>
2024-06-03 13:39:14 +0200

    Initial commit

---
+++ src/ijvm.c
@@ -0,0 +1,117 @@
+#include <stdio.h>  // for getc, printf
+#include <stdlib.h> // malloc, free
+#include "ijvm.h"
+#include "util.h" // read this file for debug prints, endianness helper functions
+
+
+// see ijvm.h for descriptions of the below functions
+
+ijvm* init_ijvm(char *binary_path, FILE* input , FILE* output) 
+{
+  // do not change these first three lines
+  ijvm* m = (ijvm *) malloc(sizeof(ijvm));
+  // note that malloc gives you memory, but gives no guarantees on the initial
+  // values of that memory. It might be all zeroes, or be random data.
+  // It is hence important that you initialize all variables in the ijvm
+  // struct and do not assume these are set to zero.
+  m->in = input;
+  m->out = output;
+  
+  // TODO: implement me
+
+  return m;
+}
+
+void destroy_ijvm(ijvm* m) 
+{
+  // TODO: implement me
+
+  free(m); // free memory for struct
+}
+
+byte_t *get_text(ijvm* m) 
+{
+  // TODO: implement me
+  return NULL;
+}
+
+unsigned int get_text_size(ijvm* m) 
+{
+  // TODO: implement me
+  return 0;
+}
+
+word_t get_constant(ijvm* m,int i) 
+{
+  // TODO: implement me
+  return 0;
+}
+
+unsigned int get_program_counter(ijvm* m) 
+{
+  // TODO: implement me
+  return 0;
+}
+
+word_t tos(ijvm* m) 
+{
+  // this operation should NOT pop (remove top element from stack)
+  // TODO: implement me
+  return -1;
+}
+
+bool finished(ijvm* m) 
+{
+  // TODO: implement me
+  return false;
+}
+
+word_t get_local_variable(ijvm* m, int i) 
+{
+  // TODO: implement me
+  return 0;
+}
+
+void step(ijvm* m) 
+{
+  // TODO: implement me
+
+}
+
+byte_t get_instruction(ijvm* m) 
+{ 
+  return get_text(m)[get_program_counter(m)]; 
+}
+
+ijvm* init_ijvm_std(char *binary_path) 
+{
+  return init_ijvm(binary_path, stdin, stdout);
+}
+
+void run(ijvm* m) 
+{
+  while (!finished(m)) 
+  {
+    step(m);
+  }
+}
+
+
+// Below: methods needed by bonus assignments, see ijvm.h
+// You can leave these unimplemented if you are not doing these bonus 
+// assignments.
+
+int get_call_stack_size(ijvm* m) 
+{
+   // TODO: implement me if doing tail call bonus
+   return 0;
+}
+
+
+// Checks if reference is a freed heap array. Note that this assumes that 
+// 
+bool is_heap_freed(ijvm* m, word_t reference) 
+{
+   // TODO: implement me if doing garbage collection bonus
+   return 0;
+}
